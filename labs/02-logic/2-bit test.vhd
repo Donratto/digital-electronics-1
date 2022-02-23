ab8ea0ac794b135096a72cdde9783f3c45f1d638
Donratto <75967197+Donratto@users.noreply.github.com>
2022-02-23 22:07:17 +0100

    4 and 2 bit vhd

---
+++ labs/02-logic/2-bit test.vhd
@@ -0,0 +1,221 @@
+------------------------------------------------------------
+--
+-- Testbench for 2-bit binary comparator.
+-- EDA Playground
+--
+-- Copyright (c) 2020-Present Tomas Fryza
+-- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------
+entity tb_comparator_2bit is
+    -- Entity of testbench is always empty
+end entity tb_comparator_2bit;
+
+------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------
+architecture testbench of tb_comparator_2bit is
+
+    -- Local signals
+    signal s_a           : std_logic_vector(2 - 1 downto 0);
+    signal s_b           : std_logic_vector(2 - 1 downto 0);
+    signal s_B_greater_A : std_logic;
+    signal s_B_equals_A  : std_logic;
+    signal s_B_less_A    : std_logic;
+
+begin
+    -- Connecting testbench signals with comparator_2bit
+    -- entity (Unit Under Test)
+    uut_comparator_2bit : entity work.comparator_2bit
+        port map(
+            a_i           => s_a,
+            b_i           => s_b,
+            B_greater_A_o => s_B_greater_A,
+            B_equals_A_o  => s_B_equals_A,
+            B_less_A_o    => s_B_less_A
+        );
+
+    --------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------
+    p_stimulus : process
+    begin
+        -- Report a note at the beginning of stimulus process
+        report "Stimulus process started" severity note;
+
+        -- First test case ...
+        s_b <= "00"; s_a <= "00"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '1') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 00, 00 FAILED" severity error;
+
+        -- Second test case ...
+        s_b <= "00"; s_a <= "01"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '1'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 00, 01 FAILED" severity error;
+        
+        -- Thrid test case ...
+        s_b <= "00"; s_a <= "10"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '1'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 00, 10 FAILED" severity error;
+        
+        -- Fourth test case ...
+        s_b <= "00"; s_a <= "11"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '1'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 00, 11 FAILED" severity error;
+        
+        -- Fifth test case ...
+        s_b <= "01"; s_a <= "00"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '1') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 10, 00 FAILED" severity error;
+        
+        -- Sixth test case ...
+        s_b <= "01"; s_a <= "01"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '1') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 01, 01 FAILED" severity error;
+
+        -- Seventh test case ...
+        s_b <= "01"; s_a <= "10"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '1'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 01, 10 FAILED" severity error;
+        
+        -- Eighth test case ...
+        s_b <= "01"; s_a <= "11"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '1'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 01, 11 FAILED" severity error;
+
+        -- Nineth test case ...
+        s_b <= "10"; s_a <= "00"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '1') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 10, 00 FAILED" severity error;
+
+        -- Tenth test case ...
+        s_b <= "10"; s_a <= "01"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '1') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 10, 01 FAILED" severity error;
+
+        -- Eleventh test case ...
+        s_b <= "10"; s_a <= "10"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '1') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 10, 10 FAILED" severity error;
+
+        -- Twelveth test case ...
+        s_b <= "10"; s_a <= "11"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '1'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 10, 11 FAILED" severity error;
+
+        -- Thirteenth test case ...
+        s_b <= "11"; s_a <= "00"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '1') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 11, 00 FAILED" severity error;
+
+        -- Fourteenth test case ...
+        s_b <= "11"; s_a <= "01"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '1') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 11, 01 FAILED" severity error;
+
+        -- Fifteenth test case ...
+        s_b <= "11"; s_a <= "10"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '1') and
+                (s_B_equals_A  = '0') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 11, 10 FAILED" severity error;
+
+        -- Sixteenth test case ...
+        s_b <= "11"; s_a <= "11"; wait for 100 ns;
+        -- ... and its expected outputs
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '1') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        -- If true, then do not report anything
+        report "Input combination 11, 11 FAILED" severity error;
+
+
+        -- Report a note at the end of stimulus process
+        report "Stimulus process finished" severity note;
+        wait; -- Data generation process is suspended forever
+    end process p_stimulus;
+
+end architecture testbench;
+
